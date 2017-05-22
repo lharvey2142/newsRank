e37a7544f79b520eef68a0d0beb820e7217ff0e3
ssdeol <sukhideol99@yahoo.com>
2017-05-22 14:26:45 -0700

    Add files via upload
    This is a search query that will get the links from google.com. 
    Link it to the search bar file I uploaded earlier and pass the query's into this file and you will get all the links from google pertaining to the term searched. Then you can do whatever you want with that

---
+++ personal/newsrank_search.py
@@ -0,0 +1,51 @@
+#link this file to the search bar file I created before. 
+# Pass the query into getGoogleLinks()function with the number 0.
+# example:  query = Trump, getGoogleLinks("Trump", "0")
+
+
+import urllib
+import mechanize
+from bs4 import BeautifulSoup
+import re
+
+
+def getGoogleLinks(link, depth):
+
+# create the browser and change the useragent
+	br = mechanize.Browser()
+	br.set_handle_robots(False)	# We don't want our browser to be seen as robotic script
+	br.addheaders = [('User-agent','chrome')]
+
+# replace space with +, look up the word in google, and return 100 links
+	term = link.replace(" ","+")
+	query = "http://www.google.com/search?num=100&q="+term+"&start="+depth
+
+	htmltext = br.open(query).read()
+
+	soup = BeautifulSoup(htmltext)
+
+	search = soup.findAll('div', attrs = {'id':'search'})
+
+	searchtext = str(search[0])
+
+	soup1 = BeautifulSoup(searchtext)
+	list_items = soup1.findAll('li')
+
+	regex = "q(?!.*q).*?&amp"	#splitting the text so the it would direct to the website
+	pattern = re.compile(regex)
+
+	results_array = []
+
+	for li in list_items:
+		soup2 = BeautifulSoup(str(li))
+		links = soup2.findAll('a')
+		source_link = links[0]
+		source_url = re.findall(pattern, str(source_link))
+		if len(source_url)>0:
+			results_array.append(str(source_url[0].replace("q=","").replace("&amp",""))
+
+	return results_array 
+
+
+print getGoogleLinks( "Donald Trump","0") #the 0, will give you the first 100 links. if you change
+										  # to 100 then it will give you the next 100 links
