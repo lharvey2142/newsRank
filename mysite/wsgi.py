2c9965d903a7407a5ca1822e948ee291c3ccdb92
Froylan Valencia <froyvalencia@gmail.com>
2017-05-22 03:32:20 -0700

    modify wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+'''
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+application = get_wsgi_application()
+'''
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
+from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")#
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
