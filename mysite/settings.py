a571a977434ecf790011dd0c8cd648ed589159cb
Froylan Valencia <froyvalencia@gmail.com>
2017-05-22 03:20:53 -0700

    modify settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,5 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
