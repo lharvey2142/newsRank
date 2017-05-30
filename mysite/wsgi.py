bda2d4cfea8c9cd0f2d624a6f4692c7114bac911
Froylan Valencia <froyvalencia@gmail.com>
2017-05-30 15:51:36 -0700

    first try to fix heroku

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -17,13 +17,12 @@ application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-path =
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #'/Users/froyvalencia/Desktop/newsRank/mysite'
-sys.path.append(os.path.join(BASE_DIR, 'newsRank', 'mysite')
+#sys.path.append(os.path.join(BASE_DIR, 'newsRank', 'mysite')
 #if path not in sys.path:
 #    sys.path.append(path)
-django.setup()
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
