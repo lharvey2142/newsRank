5c978c6291e8e4dfb649eeaee363154adae0a476
Froylan Valencia <froyvalencia@gmail.com>
2017-05-30 19:35:27 -0700

    added extract.html to display contents of url submitted to form on main page as well as store contents to database

--- newsApp/urls.py
+++ newsApp/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [ 
     #url(r'^$', 'hello fill in later',template_name="newsApp/newsApp.html")),
-    url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by("-date")[:25],template_name="newsApp/newsApp.html")),
+    url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by("-date")[:50],template_name="newsApp/newsApp.html")),
     url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Article,template_name="newsApp/article.html")),
+    url(r'^extracted$', views.extract, name="extract"),
+    #url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Article,template_name="newsApp/extract.html")),
     ]
--- newsApp/views.py
+++ newsApp/views.py
@@ -1,2 +1,27 @@
 from django.shortcuts import render
 # Create your views here.
+
+from django.core.urlresolvers import reverse
+
+import sys
+sys.path.append('/Users/froyvalencia/Desktop/newsRank')
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','mysite.settings')
+import django
+django.setup()
+
+from django.contrib.auth.models import User
+users = User.objects.all()
+from newsApp.models import Article
+
+def extract(request):
+    print(request.GET.get('url_to_clean'))
+    import newspaper
+	#from newspaper import Article
+    a = newspaper.Article(request.GET.get('url_to_clean'))
+    a.download()
+    a.parse()
+    a.nlp()
+    article = Article(address = a.url,title = a.title,body = a.text,date = a.publish_date)
+    article.save()
+    return render(request, 'newsApp/extract.html', {'title': a.title,'authors':a.authors,'text': a.text,'publish_date': a.publish_date,'keywords':a.keywords,'summary':a.summary,'videos':a.movies,'html':a.html,'top_image':a.top_image})
