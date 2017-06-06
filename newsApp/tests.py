1c35bfe456f3c94e79f66684170593e90b9342a5
Froylan Valencia <froyvalencia@gmail.com>
2017-06-06 00:23:18 -0700

    move review 3 to docs

--- newsApp/tests.py
+++ newsApp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
 # Create your tests here.
+from newsApp.models import Article
+
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        Article.objects.create(address="http://www.foxbusiness.com/markets/2017/07/01/sears-says-some-kmart-customer-credit-card-numbers-compromised.html")
+        Article.objects.create(address="http://www.theonion.com/")
+
+    def test_article_is_reliable(self):
+        """Articles that can speak are correctly identified"""
+        a1 = Article.objects.get(address="http://www.foxbusiness.com/markets/2017/07/01/sears-says-some-kmart-customer-credit-card-numbers-compromised.html")
+        #
+	    self.assertEqual(a1.result,'reliable')
+
+    def test_article_is_unreliable(self):
+    	a1 = Article.objects.get(address="http://www.theonion.com/")
+    	#
+    	self.assertEqual(a1.result,'unreliable')
+
