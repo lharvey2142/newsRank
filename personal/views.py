ee58c0368dcec89e66644d02daa9601413ac56c5
Froy Valencia <froyvalencia@users.noreply.github.com>
2018-02-28 10:22:31 -0800

    Update views.py

--- personal/views.py
+++ personal/views.py
@@ -3,7 +3,10 @@ def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
-    return render(request, 'personal/basic.html',{'content':['If you would like to contact NewsRank, please email me.','Insert Email Here']})
+    return render(request, 'personal/basic.html',{'content':['If you would like to contact NewsRank, please email me.',
+                                                             'Freshworks.help@gmail.com'
+                                                            ]})
+                                                          
 
 def googleSearch(request):
     return render(request, 'personal/googleSearch.html')
